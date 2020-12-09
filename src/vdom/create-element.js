0319b6f20994ff9d637c62dc3bd91a72467b4306
IanJiang0817 <yang3973@gmail.com>
2020-12-09 14:50:32 +0800

    render textNode

--- src/vdom/create-element.js
+++ src/vdom/create-element.js
@@ -7,6 +7,45 @@ module.exports.createElement = function (tag, data, children) {
     }
 
     if (typeof tag === 'string') {
+        if (Array.isArray(children)) {
+            for (let i = 0; i < children.length; i++) {
+                if (typeof children[i] === 'string') {
+                    children[i] = new VNode(
+                        undefined,
+                        undefined,
+                        undefined,
+                        children[i]
+                    );
+                }
+            }
+        }
         return new VNode(tag, data, children);
     }
 };
+
+//<div id="1">
+//    <span>Hello</span>
+//    <span>world!</span>
+//</div>
+
+// render(h) {
+//     return h("div", {
+//       "attrs": {
+//         "id": "1"
+//       }
+//     }, [h("span", ["Hello"]), h("span", ["world!"])]);
+// }
+
+//<div id="1">
+//    fuck
+//    <span>Hello</span>
+//    <span>world!</span>
+//</div>
+
+// render(h) {
+//     return h("div", {
+//       "attrs": {
+//         "id": "1"
+//       }
+//     }, ["fuck", h("span", ["Hello"]), h("span", ["world!"])]);
+// }
